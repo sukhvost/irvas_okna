e096415680e5d888ee0b8dab633f525b7163fc8c
Melnikov Viktor <sukhvost@yandex.ru>
2020-05-15 23:38:28 +0300

    finish timer.js

--- src/js/main.js
+++ src/js/main.js
@@ -11,10 +11,13 @@ window.addEventListener('DOMContentLoaded', () => {
     let modalState = {
 
     };
+
+    let deadline = '2020-05-20';
     changeModalsState(modalState);
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img>img', 'do_image_more', 'inline-block');
     forms(modalState);
+    timer('.container1', deadline);
 });
